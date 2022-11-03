b2bc6683164e33ecab8b72048ceeca82f12cf5ac
itlabs <itlabs@WHB019_8.it.farmingdale.edu>
2022-11-03 14:57:15 -0400

    first

---
+++ src/main/java/com/mycompany/mavenproject11/App.java
@@ -0,0 +1,30 @@
+package com.mycompany.mavenproject11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
